bfb9585508a14ff3a9bb1111a711d11e61767ae2
DhruvShah <dhruvshah2000@hotmail.com>
2019-12-05 13:57:49 -0600

    added clean-css

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 const { src, dest, parallel, series, watch } = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
+let cleancss = require('gulp-clean-css');
+
 
 function css() {
   return src('./src/css/*.css')
     .pipe(autoprefixer())
+    .pipe(cleancss())
     .pipe(dest('./dest/css'))
 }
 
