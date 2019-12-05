a8065655cdf2223be620670ea93f36f9963998b9
DhruvShah <dhruvshah2000@hotmail.com>
2019-12-05 14:37:34 -0600

    minifying images

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const { src, dest, parallel, series, watch } = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
 let cleancss = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
 
 
 function css() {
@@ -10,4 +11,11 @@ function css() {
     .pipe(dest('./dest/css'))
 }
 
+function images() {
+  return src('./src/images/*')
+    .pipe(imagemin({ verbose: true }))
+    .pipe(dest('./dest/images'))
+}
+
 exports.css = css;
+exports.images = images;
